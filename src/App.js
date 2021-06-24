0034593b8913f84ff171d7763e1a1bc6124b64c5
dualipaenr <you@example.com>
2021-06-24 14:32:36 +0530

    my first initial commit nav bar

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import "./App.css";
+import React from "react";
+
+// components
+import MyNavbar from "./components/navbar/my-navbar";
+
+const App = () => {
+  return (
+    <div>
+      <MyNavbar />
+      my portfolio
+    </div>
+  );
+};
+
+export default App;
